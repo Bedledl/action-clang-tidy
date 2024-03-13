f84144f0d2d591d4a4571ae7fb99761d16081418
Bettina Riedl <riedl.bettina@freenet.de>
2024-03-13 13:23:03 +0100

    take useexstingbuild parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,14 @@
 
 # Terminate upon errors
 set -e
-# Run cmake to configure
-mkdir ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
-cd ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
-cmake ${INPUT_CMAKEOPTIONS} ..
-cd ..
+
+if [ "${INPUT_USEEXISTINGBUILD}" == "false" ]; then
+    # Run cmake to configure
+    mkdir ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
+    cd ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
+    cmake ${INPUT_CMAKEOPTIONS} ..
+    cd ..
+fi
+
 # Run tidy script
 python3 /run-clang-tidy.py "${INPUT_SCANDIR}" "${INPUT_BUILDDIR}" "${INPUT_EXCLUDEDIRS}" "${INPUT_EXTENSIONS}"
